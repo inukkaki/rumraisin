37e7b9aea8fe210f3ea7b04901796651132ab642
inukkaki <kgn.gkrk.rstb@gmail.com>
2023-09-22 18:46:59 +0900

    Add a KeyboardHandler instance to MainLoop()

--- src/controllers/loop.cc
+++ src/controllers/loop.cc
@@ -3,6 +3,7 @@
 #include "SDL2/SDL.h"
 
 #include "interfaces/config.h"
+#include "interfaces/keyboard.h"
 #include "interfaces/timer.h"
 
 // just for debugging
@@ -10,13 +11,19 @@
 
 namespace {
 
-bool HandleEvents() {
+bool HandleEvents(KeyboardHandler& kbd_handler) {
     bool app_is_running = true;
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
         case SDL_QUIT:
             app_is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            kbd_handler.HandleKeyDown(event.key.keysym.sym);
+            break;
+        case SDL_KEYUP:
+            kbd_handler.HandleKeyUp(event.key.keysym.sym);
             break;
         default:
             break;
@@ -28,6 +35,9 @@ bool HandleEvents() {
 }  // namespace
 
 void MainLoop(bool& app_is_running) {
+    // Handle the keyboard
+    KeyboardHandler kbd_handler;
+
     // Handle the frame rate on this loop
     FrameRateBalancer fr_balancer(GetFrameRate());
     FrameRateMeasurer fr_measurer;
@@ -38,13 +48,16 @@ void MainLoop(bool& app_is_running) {
     fr_measurer.SetTimer();
     while (app_is_running) {
         // Handle events
-        app_is_running = HandleEvents();
+        app_is_running = HandleEvents(kbd_handler);
 
         // Measure the frame rate
         if (fr_measurer.MeasureFrameRate(measured_frame_rate)) {
             // just for debugging
             std::cout << measured_frame_rate << " FPS" << std::endl;
         }
+
+        // Update the keyboard handler
+        kbd_handler.Update();
 
         // Balance the frame rate
         fr_balancer.Delay();
