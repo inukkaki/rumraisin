c70e4faecfe261a19adee67d0a8db07520f761ee
inukkaki <kgn.gkrk.rstb@gmail.com>
2023-09-22 21:27:14 +0900

    Create Vector2D struct

--- include/models/general.h
+++ include/models/general.h
@@ -1,4 +1,13 @@
 #ifndef RUMRAISIN_MODELS_GENERAL_H_
 #define RUMRAISIN_MODELS_GENERAL_H_
 
+struct Vector2D {
+    float x;
+    float y;
+
+    Vector2D() : x(0.0f), y(0.0f) {}
+    Vector2D(float u, float v) : x(u), y(v) {}
+    ~Vector2D() {}
+};
+
 #endif  // RUMRAISIN_MODELS_GENERAL_H_
