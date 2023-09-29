e5ab274c7a0a72f76eca1e1202989490dfa48b53
inukkaki <kgn.gkrk.rstb@gmail.com>
2023-09-29 16:05:49 +0900

    Create EResource struct

--- include/models/entity.h
+++ include/models/entity.h
@@ -1,4 +1,15 @@
 #ifndef RUMRAISIN_MODELS_ENTITY_H_
 #define RUMRAISIN_MODELS_ENTITY_H_
 
+#include "models/general.h"
+
+struct EResource {
+    float width;   // px
+    float height;  // px
+
+    Vector2D r;  // px
+    Vector2D v;  // px frame-1
+    Vector2D a;  // px frame-2
+};
+
 #endif  // RUMRAISIN_MODELS_ENTITY_H_
