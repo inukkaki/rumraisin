ede102a03778a4085fdc871818eaccc39fdf7dc8
inukkaki <kgn.gkrk.rstb@gmail.com>
2023-10-02 00:36:34 +0900

    Create Tile struct and GetTile()

--- include/models/tile.h
+++ include/models/tile.h
@@ -1,4 +1,14 @@
 #ifndef RUMRAISIN_MODELS_TILE_H_
 #define RUMRAISIN_MODELS_TILE_H_
 
+inline constexpr int kNumberOfTileIDs = 3;
+
+inline constexpr int kUnknownTileID = 1;
+
+struct Tile {
+    //
+};
+
+const Tile& GetTile(unsigned int tile_id);
+
 #endif  // RUMRAISIN_MODELS_TILE_H_
--- src/models/tile.cc
+++ src/models/tile.cc
@@ -1,1 +1,17 @@
 #include "models/tile.h"
+
+#include <array>
+
+namespace {
+
+std::array<Tile, kNumberOfTileIDs> tile_dict;
+
+}  // namespace
+
+const Tile& GetTile(unsigned int tile_id) {
+    if (tile_id < kNumberOfTileIDs) {
+        return tile_dict[tile_id];
+    } else {
+        return tile_dict[kUnknownTileID];
+    }
+}
