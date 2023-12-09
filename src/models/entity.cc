fbc5b74510ae7d747545589e1567867fee580953
inukkaki <kgn.gkrk.rstb@gmail.com>
2023-12-10 03:05:49 +0900

    Remove CollideWithGridsBelow() and MeetGridsBelow()

--- src/models/entity.cc
+++ src/models/entity.cc
@@ -42,68 +42,6 @@ void BAddAToV::UpdateV(EResource& self) const {
 
 namespace {
 
-bool CollideWithGridsBelow(
-        EResource& res, const Field& field, int row, int col) {
-    /*
-    bool collides = false;
-    switch (field.GetCollision(row, col).coll_id) {
-    case TileCollisionId::kNone:
-        break;
-    case TileCollisionId::kSolid: {
-        int anchor_y = res.r.y + res.height;
-        int floor_y = kGridUnit * row;
-        if ((anchor_y <= floor_y) && (floor_y <= anchor_y + res.v.y)) {
-            res.v.y = floor_y - anchor_y;
-        }
-        break;
-    }
-    default:
-        break;
-    }
-    return collides;
-    */
-}
-
-void MeetGridsBelow(EResource& res, const Field& field) {
-    /*
-    assert(res.v.y > 0.0f);
-    float anchor_y = res.r.y + res.height;
-    int s_row = static_cast<int>(std::floor(anchor_y / kGridUnit));
-    int e_row = static_cast<int>(std::floor((anchor_y + res.v.y) / kGridUnit));
-    int s_col, e_col;
-    float k1, k2;
-    bool collides;
-    for (int i = s_row; i <= e_row; ++i) {
-        k1 = std::clamp(kGridUnit*(i + 1) - anchor_y, 0.0f, res.v.y);
-        k2 = std::clamp(kGridUnit*i - anchor_y, 0.0f, res.v.y);
-        if (res.v.x < 0.0f) {
-            s_col = static_cast<int>(
-                std::floor((res.r.x + k1/res.v.y*res.v.x) / kGridUnit));
-            e_col = static_cast<int>(
-                std::floor(
-                    (res.r.x + k2/res.v.y*res.v.x + res.width)
-                    / kGridUnit));
-        } else {
-            s_col = static_cast<int>(
-                std::floor((res.r.x + k2/res.v.y*res.v.x) / kGridUnit));
-            e_col = static_cast<int>(
-                std::floor(
-                    (res.r.x + k1/res.v.y*res.v.x + res.width)
-                    / kGridUnit));
-        }
-        for (int j = s_col; j <= e_col; ++j) {
-            collides = CollideWithGridsBelow(res, field, i, j);
-            if (res.v.y <= 0.0f) {
-                break;
-            }
-        }
-        if (collides) {
-            break;
-        }
-    }
-    */
-}
-
 bool CollideDownward(EResource& res, float border_y) {
     assert(res.v.y > 0.0f);
     bool collides = false;
